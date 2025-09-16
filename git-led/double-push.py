f2f8db14972af42791f7bd922560b0069b23188d
PelevinPA <pelevin.pa@phystech.edu>
2025-09-16 16:27:37 +0300

    git add .!

--- git-led/double-push.py
+++ git-led/double-push.py
@@ -8,7 +8,20 @@ up = 9
 GPIO.setup(up, GPIO.IN)
 down = 10
 GPIO.setup(down, GPIO.IN)
+num = 0
+def dec2bin(value):
+    return [int(element) for element in bin(value)[2:].zfill(8)]
+sleep_time = 0.2
 while True:
+    if (GPIO.input(up) and num < 255):
+        num = num + 1
+        print(num, dec2bin(num))
+        time.sleep(sleep_time)
+    if (GPIO.input(down) and num > 0):
+        num = num - 1
+        print(num, dec2bin(num))
+        time.sleep(sleep_time)
+    GPIO.output(leds, dec2bin(num))
     if (GPIO.input(up) and GPIO.input(down)):
         GPIO.output(leds, 1)
     else:
